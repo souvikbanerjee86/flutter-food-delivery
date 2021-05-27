17e5999c440b5bdc0c4b9cdbd6cf658f0d00498c
Souvik Banerjee <souvik.mlindia@gmail.com>
2021-05-27 15:53:10 +0100

    Day-3 Completed

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/models/order.dart';
+
+class CartScreen extends StatefulWidget {
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  _buildCartItem(Order order) {
+    return Container(
+      height: 160.0,
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(order.food.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w600),
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Cart (${currentUser.cart.length})',
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int Index) {
+            Order order = currentUser.cart[Index];
+            return _buildCartItem(order);
+          },
+          separatorBuilder: (context, index) {
+            return Divider(
+              color: Colors.grey,
+              height: 1.0,
+            );
+          },
+          itemCount: currentUser.cart.length),
+    );
+  }
+}
